cb54a3799f89430fe4d99aef1b12a9ce9306b9fd
Luke Pierotti <lpierotti2@gmail.com>
2017-06-19 21:56:09 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,19 @@
+require "pry"
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  results = {}
+  data.each do |characteristic, info|
+    info.each do |trait, names|
+      names.each do |name|
+        if results[name] == nil
+          results[name] = {}
+          results[name][characteristic] = [trait.to_s]
+        elsif results[name][characteristic] != nil
+          results[name][characteristic] << trait.to_s
+        else
+          results[name][characteristic] = [trait.to_s]
+        end
+      end
+    end
+  end
+  results
 end
